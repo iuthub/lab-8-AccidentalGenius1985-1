1a38bbe54bf713f545fcd25666abb0567e30269b
AccidentalGenius1985 <71958793+AccidentalGenius1985@users.noreply.github.com>
2021-04-25 22:25:24 +0500

    Add files via upload

---
+++ storage/framework/views/8cd5fc84cd944f94a7a91fb69c28e1a300de267b.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="#" class="navbar-brand">Laravel Guide</a>
+            <ul class="nav navbar-nav">
+                <li><a href="#">Blog</a></li>
+                <li><a href="<?php echo e(route('other.about')); ?>">About</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<?php /**PATH C:\Users\David\Desktop\INHA (2 course)\Semester ll\Internet Programming\labs\lab-8-Dooweed-1\resources\views/partials/header.blade.php ENDPATH**/ ?>
